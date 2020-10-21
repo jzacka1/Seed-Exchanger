9483761a62f4547751007d15b4fe8cc6a6a921fa
James Zacka <jameszacka@gmail.com>
2020-10-20 22:55:48 -0400

    Added seeds component.

--- Seed Exchanger/ClientApp/src/app/app.module.ts
+++ Seed Exchanger/ClientApp/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { ApiAuthorizationModule } from 'src/api-authorization/api-authorization.module';
 import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
 import { AuthorizeInterceptor } from 'src/api-authorization/authorize.interceptor';
+import { SeedsComponent } from './seeds/seeds.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
@@ -22,6 +23,7 @@ const routes: Routes = [
     children: [
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'seeds', component: SeedsComponent },
     ]
   },
   
